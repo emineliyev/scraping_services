2be235d474eb3a66fcd09c4bae2d7143bf441c89
emineliyev <emineliyev87@gmail.com>
2022-07-04 02:09:30 +0400

    production.py update2

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -36,7 +36,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["etech-scrap.herokuapp.com"]
 
 # Application definition
 
